a735f6bf2019bb452a5fe8409c13accd7a42afad
zakaria130224 <provincialkid@gmail.com>
2015-04-25 09:03:34 -0700

    all panel DragEnter implemented...

--- PanelSwap/Form1.Designer.cs
+++ PanelSwap/Form1.Designer.cs
@@ -47,17 +47,21 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.DarkKhaki;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(51, 136);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 100);
             this.panel1.TabIndex = 0;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(75, 45);
+            this.label1.Location = new System.Drawing.Point(3, 58);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 0;
@@ -66,6 +70,8 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Salmon;
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel2.Controls.Add(this.label2);
             this.panel2.Location = new System.Drawing.Point(328, 136);
             this.panel2.Name = "panel2";
@@ -73,11 +79,12 @@
             this.panel2.TabIndex = 1;
             this.panel2.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel2_DragDrop);
             this.panel2.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel2_DragEnter);
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(69, 45);
+            this.label2.Location = new System.Drawing.Point(3, 58);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 13);
             this.label2.TabIndex = 0;
@@ -86,18 +93,21 @@
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.LightGreen;
+            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel3.Controls.Add(this.label3);
-            this.panel3.Location = new System.Drawing.Point(51, 310);
+            this.panel3.Location = new System.Drawing.Point(51, 293);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(200, 100);
             this.panel3.TabIndex = 0;
             this.panel3.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel3_DragDrop);
             this.panel3.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel3_DragEnter);
+            this.panel3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseDown_1);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(75, 45);
+            this.label3.Location = new System.Drawing.Point(3, 55);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(57, 13);
             this.label3.TabIndex = 0;
@@ -106,18 +116,21 @@
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.RosyBrown;
+            this.panel4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel4.BackgroundImage")));
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel4.Controls.Add(this.label4);
-            this.panel4.Location = new System.Drawing.Point(328, 310);
+            this.panel4.Location = new System.Drawing.Point(328, 293);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(200, 100);
             this.panel4.TabIndex = 1;
             this.panel4.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel4_DragDrop);
             this.panel4.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel4_DragEnter);
+            this.panel4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseDown);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(69, 45);
+            this.label4.Location = new System.Drawing.Point(3, 55);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(57, 13);
             this.label4.TabIndex = 0;
@@ -137,7 +150,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Teal;
-            this.ClientSize = new System.Drawing.Size(589, 462);
+            this.ClientSize = new System.Drawing.Size(589, 436);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel2);
--- PanelSwap/Form1.cs
+++ PanelSwap/Form1.cs
@@ -12,7 +12,7 @@ namespace PanelSwap
 {
     public partial class Form1 : Form
     {
-        
+        System.Media.SoundPlayer swapSound = new System.Media.SoundPlayer(@"C:\Users\zakaria\Desktop\Page_Turn_Sounds_3.wav");
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +22,7 @@ namespace PanelSwap
             panel2.AllowDrop = true;
             panel3.AllowDrop = true;
             panel4.AllowDrop = true;
+            panel1.AllowDrop = true;
         }
 
         private void panel1_MouseDown(object sender, MouseEventArgs e)
@@ -36,6 +37,9 @@ namespace PanelSwap
 
         private void panel2_DragDrop(object sender, DragEventArgs e)
         {
+           // Panel p=new Panel();
+            //p.Location=new Point(e.X,e.Y);
+
             panelSwap(panel1, panel2);
 
         }
@@ -44,6 +48,8 @@ namespace PanelSwap
 
         private void panel3_DragDrop(object sender, DragEventArgs e)
         {
+           // Panel p = new Panel();
+           // p.Location = new Point(e.X, e.Y);
             panelSwap(panel1, panel3);
             panelSwap(panel2, panel3);
 
@@ -72,6 +78,34 @@ namespace PanelSwap
             Point temp = new Point(b.Location.X, b.Location.Y);
             b.Location = new Point(a.Location.X, a.Location.Y);
             a.Location = temp;
+            swapSound.Play();
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            panel2.DoDragDrop(panel1, DragDropEffects.All);
+        }
+        
+    
+        private void panel3_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            panel3.DoDragDrop(panel1, DragDropEffects.All);
+        }
+
+        private void panel4_MouseDown(object sender, MouseEventArgs e)
+        {
+            panel4.DoDragDrop(panel1, DragDropEffects.All);
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+          
+           panelSwap(panel1, panel2);
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
         }
 
     }
